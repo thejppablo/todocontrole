7cb693a88257e4544adedccdb0e3683167d05c2f
João Pedro Almeida <joaopedro071298@gmail.com>
2024-05-09 03:31:29 -0300

    changes made

---
+++ lib/src/models/item_model.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+class ItemModel{
+  File image;
+  String name;
+  ItemModel({required this.image, required this.name});
+}
---
+++ lib/src/widgets/create_image_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+class CreatedImageWidget extends StatefulWidget {
+  const CreatedImageWidget({super.key});
+
+  @override
+  State<CreatedImageWidget> createState() => _CreatedImageWidgetState();
+}
+
+class _CreatedImageWidgetState extends State<CreatedImageWidget> {
+final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
