7fd83068ab7798ca16c3122b438f8aa0f6220c1e
Mablemb <mablemb@gmail.com>
2024-07-04 22:49:31 -0300

    State/File Saving/Loading
    O app agora salva o estado em que foi encerrado

--- lib/src/pages/image_test_page.dart
+++ lib/src/pages/image_test_page.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todocontrole/src/models/models.dart';
 import 'package:todocontrole/src/widgets/app_bar_widget.dart';
 import 'package:todocontrole/src/widgets/selected_image_widget.dart';
@@ -13,45 +16,59 @@ class PaginaSelecaoImagem extends StatefulWidget {
 }
 
 class _PaginaSelecaoImagemState extends State<PaginaSelecaoImagem> {
-  // Variável de controle para alternar entre o modo de edição e visualização
   bool isCustomizable = false;
+  List<ItemModel> items = [];
 
-  // Lista para armazenar os quadrados, cada um representado por um ItemModel
-  List<ItemModel> items =
-      List.generate(4, (index) => ItemModel(title: '', image: null));
+  @override
+  void initState() {
+    super.initState();
+    // Carrega os dados salvos ao iniciar a tela
+    _loadItems();
+  }
 
-  // Função para selecionar uma imagem da galeria
+  // Carrega os itens salvos do SharedPreferences
+  Future<void> _loadItems() async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String>? itemsJson = prefs.getStringList('items');
+
+    if (itemsJson != null) {
+      setState(() {
+        items = itemsJson.map((itemJson) => ItemModel.fromJson(itemJson)).toList();
+      });
+    }
+  }
+
+  // Salva os itens no SharedPreferences
+  Future<void> _saveItems() async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String> itemsJson = items.map((item) => item.toJson()).toList();
+    await prefs.setStringList('items', itemsJson);
+  }
+
   Future<void> _selecionarImagem(int indice) async {
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
       setState(() {
-        // Atualiza a imagem do quadrado correspondente na lista de items
         items[indice].image = File(pickedFile.path);
+        _saveItems(); // Salva após a alteração
       });
     }
   }
 
-  // Função para adicionar um quadrado vazio
   void _adicionarQuadrado() {
     setState(() {
-      // Adiciona um novo ItemModel à lista de items
-      items.add(ItemModel(title: '', image: null));
+      items.add(ItemModel(image: null, title: ''));
+      _saveItems(); // Salva após a alteração
     });
   }
 
   final TextEditingController textEditingController = TextEditingController();
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(
-        // Botão na AppBar para alternar entre o modo de edição e visualização
         onPressed: () {
           setState(() {
             isCustomizable = !isCustomizable;
@@ -62,7 +79,7 @@ class _PaginaSelecaoImagemState extends State<PaginaSelecaoImagem> {
         children: [
           Expanded(
             child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
                 crossAxisSpacing: 4.0,
                 mainAxisSpacing: 4.0,
@@ -76,19 +93,16 @@ class _PaginaSelecaoImagemState extends State<PaginaSelecaoImagem> {
                       child: Center(
                         child: items[indice].image != null
                             ? SelectedImageWidget(
-                                // Exibe a imagem do quadrado se estiver presente
                                 file: items[indice].image!,
                                 text: items[indice].title,
                               )
                             : InkWell(
-                                // Ícone de adição se não houver imagem
-                                child: Icon(Icons.add),
+                                child: const Icon(Icons.add),
                                 onTap: () => _showDialog(indice),
                               ),
                       ),
                     ),
                     Visibility(
-                      // Botão de edição (lápis) visível apenas no modo de edição
                       visible: isCustomizable,
                       child: IconButton(
                         onPressed: () {
@@ -98,28 +112,25 @@ class _PaginaSelecaoImagemState extends State<PaginaSelecaoImagem> {
                           Icons.edit,
                           color: Colors.white,
                         ),
-                        style:
-                            IconButton.styleFrom(backgroundColor: Colors.black),
+                        style: IconButton.styleFrom(backgroundColor: Colors.black),
                       ),
                     ),
                     Visibility(
-                      // Botão de exclusão (X) visível apenas no modo de edição
                       visible: isCustomizable,
                       child: Positioned(
                         right: 0,
                         child: IconButton(
                           onPressed: () {
                             setState(() {
-                              // Remove o quadrado da lista de items
                               items.removeAt(indice);
+                              _saveItems(); // Salva após a alteração
                             });
                           },
                           icon: const Icon(
                             Icons.close,
                             color: Colors.white,
                           ),
-                          style:
-                              IconButton.styleFrom(backgroundColor: Colors.red),
+                          style: IconButton.styleFrom(backgroundColor: Colors.red),
                         ),
                       ),
                     ),
@@ -128,47 +139,47 @@ class _PaginaSelecaoImagemState extends State<PaginaSelecaoImagem> {
               },
             ),
           ),
-          // Botão para adicionar mais quadrados vazios
           ElevatedButton(
             onPressed: _adicionarQuadrado,
-            child: Text('Adicionar Quadrado'),
+            child: const Text('Adicionar Quadrado'),
           ),
         ],
       ),
     );
   }
 
-  // Função para exibir um diálogo para inserir um título personalizado
   Future<void> _showDialog(int index) async {
     return showDialog(
-        context: context,
-        barrierDismissible: true,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("nome do quadrado"),
-            content: TextField(
-              controller: textEditingController,
-              decoration: InputDecoration(
-                  hintText: 'insira um nome', border: OutlineInputBorder()),
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Nome do Quadrado"),
+          content: TextField(
+            controller: textEditingController,
+            decoration: const InputDecoration(
+              hintText: 'Insira um nome',
+              border: OutlineInputBorder(),
             ),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  if (textEditingController.text.isNotEmpty) {
-                    setState(() {
-                      // Atualiza o título do quadrado correspondente na lista de items
-                      items[index].title = textEditingController.text;
-                      textEditingController.clear();
-                    });
-                    // Seleciona uma imagem após inserir o título
-                    _selecionarImagem(index);
-                    Navigator.pop(context, 'OK');
-                  }
-                },
-                child: const Text('OK'),
-              ),
-            ],
-          );
-        });
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                if (textEditingController.text.isNotEmpty) {
+                  setState(() {
+                    items[index].title = textEditingController.text;
+                    textEditingController.clear();
+                    _saveItems(); // Salva após a alteração
+                  });
+                  _selecionarImagem(index); // Seleciona uma imagem após inserir o título
+                  Navigator.pop(context, 'OK');
+                }
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
--- lib/src/pages/pages.dart
+++ lib/src/pages/pages.dart
@@ -1,3 +1,1 @@
-export '../widgets/image_picker_example.dart';
-export 'home_page.dart';
 export 'image_test_page.dart';
